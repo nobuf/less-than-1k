3b59ff47742c85410d063b91ca90d41b73869b7c
Nobu Funaki <nobu@zuzara.com>
2011-07-19 18:16:27 -0700

    to activate @ error-control operator

--- lib/Bootstrap.php
+++ lib/Bootstrap.php
@@ -21,6 +21,9 @@ spl_autoload_register('bootstrap_autoload');
 
 //  standardize the error handling
 function exception_error_handler($errno, $errstr, $errfile, $errline) {
+    if (error_reporting() === 0) {
+        return;
+    }
     throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
 }
 set_error_handler("exception_error_handler");
